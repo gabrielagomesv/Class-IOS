152a20591f7edc99623897b19025844d9dbf9386
Usuário Convidado <Guest@L2602MICRO02.local>
2018-10-01 09:06:16 -0300

    Aula08

---
+++ Exercicio_json_e_table_view_star_wars/Aplicativo.swift
@@ -0,0 +1,14 @@
+//
+//  Aplicativo.swift
+//  Exercicio_json_e_table_view_star_wars
+//
+//  Created by Usuário Convidado on 01/10/2018.
+//  Copyright © 2018 Gabriela Gomes. All rights reserved.
+//
+
+import UIKit
+
+class Aplicativo: NSObject {
+    var nome:String = ""
+    var altura:String = ""
+}
